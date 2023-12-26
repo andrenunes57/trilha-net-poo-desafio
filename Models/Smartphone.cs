d73e81796e424d38a6cb48d965014dbcf815f7ea
André Nunes de Lima <andre.nunes@live.com>
2023-12-26 15:19:16 -0300

    Complete OOP challenge; use of the 'nameof' to dynamically get the name of the smartphone type

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,30 @@
 namespace DesafioPOO.Models
 {
-    public abstract class Smartphone
-    {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+	public abstract class Smartphone
+	{
+		public string Numero { get; set; }
+		private string _modelo { get; set; }
+		private string _imei { get; set; }
+		private int _memoria { get; set; }
 
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
+		public Smartphone(string numero, string modelo, string imei, int memoria)
+		{
+			Numero = numero;
+			_modelo = modelo;
+			_imei = imei;
+			_memoria = memoria;
+		}
 
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
+		public void Ligar()
+		{
+			Console.WriteLine("Ligando...");
+		}
 
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
+		public void ReceberLigacao()
+		{
+			Console.WriteLine("Recebendo ligação...");
+		}
 
-        public abstract void InstalarAplicativo(string nomeApp);
-    }
+		public abstract void InstalarAplicativo(string nomeApp);
+	}
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+var nokia = new Nokia("11980004000", "Lumia", "1111111111", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone IPhone");
+var iphone = new Iphone("11970003000", "X10", "2222222222", 128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
